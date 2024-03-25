0b05b6a639259cf53d94a372b5663ffc59427800
ken_ruster <ken.ruster@gmail.com>
2024-03-25 12:06:02 +0800

    Change LED logic to mask

--- ledControl.h
+++ ledControl.h
@@ -5,23 +5,23 @@ void control_RGB_LEDs(int controller, bool state) {
 	switch (controller){
 		case 0:
 			PTB->PDOR = state << RED_LED;
-			PTB->PCOR = 1 << GREEN_LED;
-			PTD->PCOR = 1 << BLUE_LED;
+			PTB->PCOR |= 1 << GREEN_LED;
+			PTD->PCOR |= 1 << BLUE_LED;
 			break;
 		case 1:
 			PTB->PDOR = state << GREEN_LED;
-			PTB->PCOR = 1 << RED_LED;
-			PTD->PCOR = 1 << BLUE_LED;
+			PTB->PCOR |= 1 << RED_LED;
+			PTD->PCOR |= 1 << BLUE_LED;
 			break;
 		case 2:
 			PTD->PDOR = state << BLUE_LED;
-			PTB->PCOR = 1 << RED_LED;
-			PTB->PCOR = 1 << GREEN_LED;
+			PTB->PCOR |= 1 << RED_LED;
+			PTB->PCOR |= 1 << GREEN_LED;
 			break;
 		default:
-			PTB->PCOR = 1 << RED_LED;
-			PTB->PCOR = 1 << GREEN_LED;
-			PTD->PCOR = 1 << BLUE_LED;
+			PTB->PCOR |= 1 << RED_LED;
+			PTB->PCOR |= 1 << GREEN_LED;
+			PTD->PCOR |= 1 << BLUE_LED;
 			break;
 	}
 }
